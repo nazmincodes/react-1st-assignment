a135d4c211b915c306e69ca47d6c504ee2fd46a3
unknown <msnazminakter690@gmail.com>
2026-09-12 09:29:29 +0600

    first commit

---
+++ src/components/Banner.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from "../assets/banner-stack.png"
+const Banner = () => {
+    return (
+        <div className='flex justify-between max-w-6xl mx-auto '>
+            <div className='py-25'>
+                <h1 className='text-4xl font-bold'>Build Your Ideal<br></br> <span className='bg-gradient-to-r
+                 from-orange-500 via-pink-500 to-purple-600 bg-clip-text text-transparent'>Development Stack</span></h1>
+                <p className='py-6'>Explore frontend, backend, database, and tooling options,<br></br>
+                compare them side by side, and put together the stack that fits your<br></br>
+                next project.</p>
+                <div className='flex gap-4'>
+                 <button
+                className="border-2 border-gray-100 rounded-2xl px-4 py-2 text-amber-50
+                bg-gradient-to-r from-orange-500 via-orange-400 to-pink-400"
+               >
+               Explore Technologies
+              </button>
+                <button className='border-2 border-gray-200 rounded-2xl px-10'>Learn More</button>
+                </div>
+
+            </div>
+            <div>
+                <img src={logo} className='h-100' alt=""/>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import logo from "../assets/logo-text.png"
+const Navbar = () => {
+ return ( <nav className="flex justify-between gap-4 max-w-6xl mx-auto py-4 border-b border-gray-100">
+    <img src={logo} className="w-[100px] h-[30px]"  alt=""/>
+    <ul className="flex gap-4 items-center">
+        <li>
+            <a href="/" className="text-pink-500">Home</a>
+        </li>
+         <li>
+            <a href="/">Technologies</a>
+        </li>
+         <li>
+            <a href="/">Projects</a>
+        </li>
+         <li>
+            <a href="/">About</a>
+        </li>
+            <li>
+            <a href="/">Contact</a>
+        </li>
+    </ul>
+    <div className="flex gap-4">
+    <button>Sign In</button>
+    <button className="bg-pink-500 border-0 rounded-3xl hover:bg-pink-300 hover:text-orange-100
+     hover:scale-110 transition-transform-duration-300 px-3 text-white">Sign Up</button>
+    </div>
+ </nav>)
+};
+export default Navbar;
